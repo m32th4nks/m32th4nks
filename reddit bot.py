7fc6b5cec30500ddc7eefccd5d43c7b16c4de2e3
m32th4nks <31380148+m32th4nks@users.noreply.github.com>
2017-08-29 10:34:34 -0500

    m32th4nks

--- reddit bot.py
+++ reddit bot.py
@@ -5,13 +5,15 @@ import random
 import config
 import thanks
 
-thanks = thanks.thanks
-
 reddit = praw.Reddit(username = config.username,
                         password = config.password,
                         client_id = config.client_id,
                         client_secret = config.client_secret,
                         user_agent = "Me too thanks bot (pls don't ban me admins...)")
+print("Logged into Reddit...")
+
+thanks = thanks.thanks
+print("Loaded 'thanks.py...'")
 
 if not os.path.isfile("comments_replied_to.txt"):
     comments_replied_to = []
@@ -19,6 +21,7 @@ else:
     with open("comments_replied_to.txt", "r") as f:
         comments_replied_to = f.read()
         comments_replied_to = comments_replied_to.split("\n")
+print("Loaded 'comments_replied_to.txt'...")
 
 commentTally = 0
 replyTally = 0
@@ -27,6 +30,7 @@ replyTally = 0
 #subreddit = reddit.subreddit("me_irl+meirl+4chan+absolutelynotme_irl+2meirl4meirl+dankmemes+fellowkids+woof_irl+hentai_irl+mildlyinteresting+showerthoughts+teenagers+memes+gaming+wholesomememes+kenM+madlads+nintendoswitch+prequelmemes+memeeconomy+deepfriedmemes+blackpeopletwitter+coaxedintoasnafu+surrealmemes")
 #subreddit = reddit.subreddit("popular")
 subreddit = reddit.subreddit("all")
+print("Preparing to stream...")
 
 for comment in subreddit.stream.comments():
     
@@ -55,6 +59,18 @@ for comment in subreddit.stream.comments():
                 f.write(comment.id + "\n")
 
         comment.reply(random.choice(thanks))
+        print("")
+        print("Replied to comment " + comment.author.name)
+        print("")
+        
+        replyTally += 1
+
+    elif "m32th4nks" in comment.body in comment.body and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+        comments_replied_to.append(str(comment.id))
+        with open ("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
+
+        comment.reply("Yup that's me ^^^too ^^^thanks")
         print("")
         print("Replied to comment " + comment.author.name)
         print("")
