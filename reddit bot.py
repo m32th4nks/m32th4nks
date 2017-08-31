521c266ec2e009b5d81326d414e44c3fe7648db1
m32th4nks <31380148+m32th4nks@users.noreply.github.com>
2017-08-31 08:24:43 -0500

    m32th4nks

--- reddit bot.py
+++ reddit bot.py
@@ -34,37 +34,48 @@ for comment in subreddit.stream.comments():
     commentTally += 1
 
     if(comment.author != reddit.user.me()):
-        print(str(commentTally) + ":" + str(replyTally) + " | " + comment.id + " | " + str(comment.author.name))
+        print(str(commentTally) + " : " + str(replyTally) +  " | " + comment.id + " | " + str(comment.author.name))
     else:
-        print(str(commentTally) + ":" + str(replyTally) + " | " + comment.id  + " | " + str(comment.author.name) + " | " + " ******")
+        print(str(commentTally) + " : " + str(replyTally) +  " | " + comment.id + " | " + str(comment.author.name) + " | " + " ******")
 
+    
     #try/except because attempting to comment in a banned sub will throw an error
     try:
+            
+        if "me too thanks" in comment.body.lower() in comment.body.lower() and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+            comments_replied_to.append(str(comment.id))
+            with open ("comments_replied_to.txt", "a") as f:
+                    f.write(comment.id + "\n")
+
+            comment.reply(random.choice(thanks))
+            replyTally += 1
+
+        elif "/u/m32th4nks" in comment.body.lower() in comment.body.lower() and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+            comments_replied_to.append(str(comment.id))
+            with open ("comments_replied_to.txt", "a") as f:
+                    f.write(comment.id + "\n")
+
+            comment.reply("Oh hey that's me ^^^too ^^^thanks")
+            replyTally += 1
+
+        elif "me two thanks" in comment.body.lower() in comment.body.lower() and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+            comments_replied_to.append(str(comment.id))
+            with open ("comments_replied_to.txt", "a") as f:
+                    f.write(comment.id + "\n")
+
+            comment.reply("Me three thanks")
+            replyTally += 1
+
+        elif "good bot" in comment.body.lower() in comment.body.lower() and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+            if comment.parent().author == reddit.user.me():
+                comment.reply("You humans make me so happy :')")
+                replyTally += 1 
+
+        elif "bad bot" in comment.body.lower() in comment.body.lower() and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
+            if comment.parent().author == reddit.user.me():
+                comment.reply("I-I didn't even like you anyways... ^:(")
+                replyTally += 1
         
-        if "Me too thanks" in comment.body in comment.body and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
-            comments_replied_to.append(str(comment.id))
-            with open ("comments_replied_to.txt", "a") as f:
-                    f.write(comment.id + "\n")
-
-            comment.reply(random.choice(thanks))
-            replyTally += 1
-            
-        elif "me too thanks" in comment.body in comment.body and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
-            comments_replied_to.append(str(comment.id))
-            with open ("comments_replied_to.txt", "a") as f:
-                    f.write(comment.id + "\n")
-
-            comment.reply(random.choice(thanks))
-            replyTally += 1
-
-        elif "m32th4nks" in comment.body in comment.body and comment.author != reddit.user.me() and comment.id not in comments_replied_to:
-            comments_replied_to.append(str(comment.id))
-            with open ("comments_replied_to.txt", "a") as f:
-                    f.write(comment.id + "\n")
-
-            comment.reply("Yup that's me ^^^too ^^^thanks")
-            replyTally += 1
-            
     except:
         pass
     
